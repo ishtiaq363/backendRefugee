1d65e8686362dc66a474dd68f6c48e36e59742e2
ishtiaq363 <imitaztkd@gmail.com>
2025-11-06 20:29:01 +0500

    minor changes

--- RefugeeSkillsPlatform.Core/DTOs/BookingListDto.cs
+++ RefugeeSkillsPlatform.Core/DTOs/BookingListDto.cs
@@ -17,12 +17,13 @@ namespace RefugeeSkillsPlatform.Core.DTOs
         public DateTime SlotEndDate { get; set; }
         public string Status { get; set; } = string.Empty;
         public DateTime BookingDate { get; set; }
-        public string? ZoomLink { get; set; } // ✅ nullable
+        public string? ZoomLink { get; set; } 
         public string Client { get; set; } = string.Empty;
         public string Provider { get; set; } = string.Empty;
         public string ServiceName { get; set; } = string.Empty;
         public string MethodName { get; set; } = string.Empty;
         public long RowNum { get; set; }
+        public string? PaymentStatus { get; set; } = string.Empty;
     }
 
 
